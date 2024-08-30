7e037c0d13f7d2b2ce4a4f73a98815dcb471d008
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-08-30 05:14:33 -0900

    wrong way of using zustand-react query done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-
+import { useQuery } from "@tanstack/react-query";
+import { getUsers } from "./api/user";
+import { useUserStore } from "./state/user-store";
+import { useEffect } from "react";
 
 function App() {
+  const {users, setUsers} = useUserStore()
+
+  const { data, isPending, isError } = useQuery({
+    queryKey: ["users"],
+    queryFn: () => getUsers(),
+  });
+
+  useEffect(() => {
+    if(data) {
+      setUsers(data)
+    }
+  }, [data])
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error...</div>;
+  }
+
+
 
   return (
-    <div className="text-2xl text-red-500" >
-      Hello World
+    <div className="">
+      {users.map((user) => (
+        <div key={user.id} >
+          {user.name}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
